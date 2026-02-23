d536df178b6e537c13825bff3aef0d830785daa9
pavanpalivela93 <pavanpalivela93@gmail.com>
2026-02-23 10:28:42 +0530

    Update example.cpp

--- constexpr/example.cpp
+++ constexpr/example.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-constexpr int fib(int n) {
+constexpr int fib(int n)
+{
     if (n <= 1)
         return n;
 
     return fib(n - 1) + fib(n - 2);
 }
 
-constexpr int add(int a, int b) {
+constexpr int add(int a, int b)
+{
     return a + b;
 }
 
-int main() {
+int main()
+{
     const int n = 11;
     constexpr int i = fib(n);
     constexpr int j = add(3, 5);
     std::cout << i << " " << j;
-
-
 
     return 0;
 }
